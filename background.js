6aa073c49bbc6ee8cd43c8a275281924737dbd33
qbug <wumqing@yeah.net>
2023-05-29 22:37:10 +0800

    feat: match url by extra search

--- background.js
+++ background.js
@@ -1,22 +1,22 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'checkHistory') {
-      const url = request.url;
-  
-      chrome.history.search({ text: url }, function (results) {
-        if (results.length > 0) {
-          const historyItem = results[0];
-          const visitCount = historyItem.visitCount;
-          const lastVisitTime = new Date(historyItem.lastVisitTime);
-          const response = {
-            visitCount: visitCount,
-            lastVisitTime: lastVisitTime.toLocaleString()
-          };
-          sendResponse(response);
-        } else {
-          sendResponse({ visitCount: 0, lastVisitTime: null });
-        }
+  if (request.action === 'checkHistory') {
+    const url = request.url;
+
+    chrome.history.search({ text: url, startTime: 0 }, function (results) {
+      let matchResults = results.filter(res => res.url === url)
+      if (results.length == 0 || matchResults.length == 0) {
+        sendResponse({ visitCount: 0, lastVisitTime: null });
+        return ;
+      }
+      const historyItem = matchResults[0];
+      const visitCount = historyItem.visitCount;
+      const lastVisitTime = new Date(historyItem.lastVisitTime);
+      sendResponse({
+        visitCount: visitCount,
+        lastVisitTime: lastVisitTime.toLocaleString()
       });
-  
-      return true; // 保持长连接以异步发送响应
-    }
-  });
+    });
+
+    return true; // 保持长连接以异步发送响应
+  }
+});
