0b59a6280a7579e99d41be877b3c9ad0c952bce1
qbug <wumqing@yeah.net>
2023-05-30 21:34:36 +0800

    feat(wip): add whitelist/blacklist settings in popup

--- popup.js
+++ popup.js
@@ -1,18 +1,37 @@
-console.log("popup js!");
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const currentTab = tabs[0];
-    const url = currentTab.url;
-    console.log("url: " + url)
-    chrome.history.search({ text: url }, function (results) {
-        let visitCount = 0;
-        let lastVisitTime = null;
-        if (results.length > 0) {
-          const historyItem = results[0];
-          visitCount = historyItem.visitCount;
-          lastVisitTime = new Date(historyItem.lastVisitTime).toLocaleString();
-        } 
-        document.getElementById('visit-count').textContent += ` ${visitCount}次`;
-        document.getElementById('last-visit-time').textContent += ` ${lastVisitTime}`;
+document.addEventListener('DOMContentLoaded', function () {
+    const whitelistRadio = document.querySelector('input[value="whitelist"]');
+    const blacklistRadio = document.querySelector('input[value="blacklist"]');
+    const saveButton = document.getElementById('saveBtn');
+    const blacklistInput = document.getElementById('blacklistInput');
+    const whitelistInput = document.getElementById('whitelistInput');
+
+    // 加载保存的设置和名单内容
+    chrome.storage.local.get(['listType', 'blacklist', 'whitelist'], function (data) {
+        const savedListType = data.listType ?? 'blacklist';
+        const savedBlacklist = data.blacklist ?? '';
+        const savedWhitelist = data.whitelist ?? '';
+
+        if (savedListType) {
+            if (savedListType === 'whitelist') {
+                whitelistRadio.checked = true;
+            } else if (savedListType === 'blacklist') {
+                blacklistRadio.checked = true;
+            }
+        }
+
+        blacklistInput.value = savedBlacklist;
+        whitelistInput.value = savedWhitelist;
     });
-  });
-  
+
+    // 监听保存按钮点击事件
+    saveButton.addEventListener('click', function () {
+        let listType = whitelistRadio.checked ? 'whitelist' : 'blacklist';
+
+        const blacklist = blacklistInput.value;
+        const whitelist = whitelistInput.value;
+
+        // 保存设置和名单内容到 chrome.storage
+        chrome.storage.local.set({ listType: listType, blacklist: blacklist, whitelist: whitelist }, function () {
+        });
+    });
+});
