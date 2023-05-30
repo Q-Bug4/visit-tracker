ac70bc7669b9269902c2b291700fb87b2e540179
qbug <wumqing@yeah.net>
2023-05-30 22:04:00 +0800

    feat: add whilelist/blacklist filter

--- background.js
+++ background.js
@@ -1,21 +1,34 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'checkHistory') {
     const url = request.url;
-
-    chrome.history.search({ text: url, startTime: 0 }, function (results) {
-      let matchResults = results.filter(res => res.url === url)
-      if (results.length == 0 || matchResults.length == 0) {
-        sendResponse({ visitCount: 0, lastVisitTime: null });
-        return ;
+    let savedListType
+    let savedBlacklist
+    let savedWhitelist
+    chrome.storage.local.get(['listType', 'blacklist', 'whitelist'], function (data) {
+      savedListType = data.listType ?? 'blacklist';
+      savedBlacklist = data.blacklist ?? '';
+      savedWhitelist = data.whitelist ?? '';
+      let shouldAbort = (savedListType == 'blacklist')
+        ? savedBlacklist !== '' && !!savedBlacklist.split("\n").find(rule => new RegExp(rule).test(url))
+        : savedWhitelist !== '' && !savedWhitelist.split("\n").find(rule => new RegExp(rule).test(url))
+      if (shouldAbort) {
+        return;
       }
-      const historyItem = matchResults[0];
-      const visitCount = historyItem.visitCount;
-      const lastVisitTime = new Date(historyItem.lastVisitTime);
-      sendResponse({
-        visitCount: visitCount,
-        lastVisitTime: lastVisitTime.toLocaleString()
+      chrome.history.search({ text: url, startTime: 0 }, function (results) {
+        let matchResults = results.filter(res => res.url === url)
+        if (results.length == 0 || matchResults.length == 0) {
+          sendResponse({ visitCount: 0, lastVisitTime: null });
+          return;
+        }
+        const historyItem = matchResults[0];
+        const visitCount = historyItem.visitCount;
+        const lastVisitTime = new Date(historyItem.lastVisitTime);
+        sendResponse({
+          visitCount: visitCount,
+          lastVisitTime: lastVisitTime.toLocaleString()
+        });
       });
-    });
+    })
 
     return true; // 保持长连接以异步发送响应
   }
